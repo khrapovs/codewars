01a55c7a1e410225b214d19441193257ed1adafa
Stanislav Khrapov <khrapovs@gmail.com>
2022-12-10 18:01:37 +0100

    Multiply

---
+++ src/multiply/multiply.h
@@ -0,0 +1,8 @@
+#ifndef CODEWARS_MULTIPLY_H
+#define CODEWARS_MULTIPLY_H
+
+int multiply(int a, int b) {
+    return a * b;
+}
+
+#endif //CODEWARS_MULTIPLY_H
---
+++ tests/test_multiply.cpp
@@ -0,0 +1,13 @@
+#include "igloo/igloo_alt.h"
+#include "multiply.h"
+
+using namespace igloo;
+
+Describe(multiply_method) {
+    It(should_handle_basic_tests) {
+        Assert::That(multiply(1, 2), Equals(2));
+        Assert::That(multiply(3, 4), Equals(12));
+        Assert::That(multiply(5, 6), Equals(30));
+        Assert::That(multiply(-1, 20), Equals(-20));
+    }
+};
