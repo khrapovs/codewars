8ceb58bf27d153851ee37cb0309f003ed1e749d7
Stanislav Khrapov <khrapovs@gmail.com>
2022-12-12 17:14:47 +0100

    reverse strings

--- tests/test_reversed_strings.cpp
+++ tests/test_reversed_strings.cpp
@@ -1,6 +1,11 @@
+#include <cstdlib>
+#include <ctime>
+#include <algorithm>
+
 #include "igloo/igloo_alt.h"
 #include "reversed_strings.h"
 
+using namespace std;
 using namespace igloo;
 
 Describe(Reverse_String) {
@@ -14,5 +19,22 @@ Describe(Reverse_String) {
         Assert::That(reverseString("codewars"), Equals("srawedoc"));
         Assert::That(reverseString("football"), Equals("llabtoof"));
         Assert::That(reverseString("translation"), Equals("noitalsnart"));
+    }
+
+    string modelSolution(string str) {
+        reverse(str.begin(), str.end());
+        return str;
+    }
+
+    It(generate_random_string) {
+        static const string charList = "abcdefghijklmnopqrstuvwxyz";
+        srand(time(0));
+
+        string alphanumeric;
+        for (int i = 0; i < 15; i++) {
+            alphanumeric += charList[rand() % 13 + 2];
+        }
+        string result = modelSolution(alphanumeric);
+        Assert::That(reverseString(alphanumeric), Equals(result));
     }
 };
