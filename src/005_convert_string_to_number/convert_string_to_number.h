4791b79f9e6c190dbc127c00d45097e49e226316
Stanislav Khrapov <khrapovs@gmail.com>
2022-12-15 17:40:09 +0100

    convert string to number

---
+++ src/005_convert_string_to_number/convert_string_to_number.h
@@ -0,0 +1,8 @@
+#ifndef CODEWARS_CONVERT_STRING_TO_NUMBER_H
+#define CODEWARS_CONVERT_STRING_TO_NUMBER_H
+
+#include <string>
+
+int string_to_number(const std::string &s);
+
+#endif //CODEWARS_CONVERT_STRING_TO_NUMBER_H
