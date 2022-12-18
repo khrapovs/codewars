fef2d4b714812d941477e1273cd90f0322114043
Stanislav Khrapov <khrapovs@gmail.com>
2022-12-18 16:11:25 +0100

    string repeat (#9)

---
+++ src/008_string_repeat/README.md
@@ -0,0 +1,3 @@
+Exercise: https://www.codewars.com/kata/57a0e5c372292dd76d000d7e/train/cpp
+
+Solution: https://www.codewars.com/kata/57a0e5c372292dd76d000d7e/solutions/cpp
---
+++ src/008_string_repeat/string_repeat.h
@@ -0,0 +1,8 @@
+#ifndef CODEWARS_STRING_REPEAT_H
+#define CODEWARS_STRING_REPEAT_H
+
+#include <string>
+
+std::string repeat_str(size_t repeat, const std::string &str);
+
+#endif //CODEWARS_STRING_REPEAT_H
