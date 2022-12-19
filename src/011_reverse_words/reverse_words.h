fba99579036ea8c65c7677963f17c9c7e23d03c8
Stanislav Khrapov <khrapovs@gmail.com>
2022-12-19 17:54:14 +0100

    reverse words

---
+++ src/011_reverse_words/reverse_words.h
@@ -0,0 +1,10 @@
+#ifndef CODEWARS_REVERSE_WORDS_H
+#define CODEWARS_REVERSE_WORDS_H
+
+#include "string"
+
+using namespace std;
+
+string reverse_words(string str);
+
+#endif //CODEWARS_REVERSE_WORDS_H
